d0dc8f90e079961e1ec5b94493c9705ba033aae6
mohamadsolouki <mohamad.solouki@gmail.com>
2023-02-20 01:03:53 +0330

    writing tests for adding, marking, and streaks calculation

--- test_habit.py
+++ test_habit.py
@@ -1,40 +1,28 @@
-import pytest
-from datetime import date, timedelta
-import habits
+import habits as hb
 import db
 
+hb = hb.Habit()
 db = db.Database()
 
 
-def test_streak_calculation():
-    # Create a HabitTracker object and add a new habit
-    habit_tracker = habits.Habit()
-    habit_tracker.add_habit("Test habit", 1)
+def test_add_habit():
+    hb.add_habit("Test habit", 1, '2023-01-01')
+    assert db.get_habit(1) == (1, "Test habit", 1, '2023-01-01', None, 0)
 
-    # Mark the habit as complete for the past 10 days
-    today = date.today()
-    for i in range(10):
-        completion_date = today - timedelta(days=i)
-        habit_tracker.mark_habit_as_complete(1, completion_date)
 
-    # Check that the current streak is 10 and the longest streak is also 10
-    db.__init__()
-    db.c.execute("SELECT current_streak, longest_streak FROM streaks WHERE habit_id=?", (1,))
-    row = db.c.fetchone()
-    assert row[0] == 10
-    assert row[1] == 10
+def test_mark_habit_as_complete_two_consecutive_days():
+    hb.mark_habit_as_complete(1, '2023-01-02')
+    hb.mark_habit_as_complete(1, '2023-01-03')
+    assert db.get_habit_completions(1) == [('2023-01-02',), ('2023-01-03',)]
 
-    # Mark the habit as incomplete for the last 3 days
-    for i in range(3):
-        completion_date = today - timedelta(days=i)
-        habit_tracker.mark_habit_as_complete(1, completion_date)
 
-    # Check that the current streak is now 0 and the longest streak is still 10
-    db.__init__()
-    db.c.execute("SELECT current_streak, longest_streak FROM streaks WHERE habit_id=?", (1,))
-    row = db.c.fetchone()
-    assert row[0] == 0
-    assert row[1] == 10
-    # habit = habit_tracker.get_habit_by_name("Test habit")
-    # assert habit.current_streak == 0
-    # assert habit.longest_streak == 10
+# test marking a daily habit as complete for 7 days which has current streak of 3 and longest streak of 4
+def test_mark_habit_as_complete():
+    hb.mark_habit_as_complete(1, "2023-01-08")
+    hb.mark_habit_as_complete(1, "2023-01-09")
+    hb.mark_habit_as_complete(1, "2023-01-10")
+    hb.mark_habit_as_complete(1, "2023-01-11")
+    hb.mark_habit_as_complete(1, "2023-01-13")
+    hb.mark_habit_as_complete(1, "2023-01-14")
+    assert db.get_streaks_for_habit(1) == (1, 1, 2, 4)
+
