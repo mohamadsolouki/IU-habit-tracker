c1e44035f94c9811436288a605bc411d67e8a2d0
mohamadsolouki <mohamad.solouki@gmail.com>
2023-02-21 00:45:13 +0330

    commenting

--- analyze.py
+++ analyze.py
@@ -20,13 +20,16 @@ def habits_todo():
         habit_name = habit[1]
         periodicity = habit[2]
         last_completion_date = habit[4]
+        # If the habit has never been completed, add it to the list
         if last_completion_date is None:
             habit_list.append([habit_id, habit_name, periodicity, "Never"])
             continue
         last_completion_date_str = last_completion_date.split(" ")[0]
         last_completion_date = datetime.strptime(last_completion_date_str, '%Y-%m-%d').date()
+        # If the habit has been completed today, skip it
         if (today - last_completion_date).days >= periodicity:
             habit_list.append([habit_id, habit_name, periodicity, last_completion_date])
+    # If there are no habits to do today, print a message and return
     if not habit_list:
         print(termcolor.colored("You don't have any habits to do today!", "red"))
         return
@@ -48,16 +51,16 @@ def habit_status():
     """
     habits = db.get_habits()
     habit_list = []
-
+    # If there are no habits, print a message and return
     if len(habits) == 0:
         print(termcolor.colored("You don't have any habits yet!", "red"))
         return
-
+    # If there are habits, add them to the habit list
     for habit in habits:
         habit_list.append(f"{habit[0]}: {habit[1]}")
     habit_to_mark = questionary.select("Which habit would you like to analyze?", choices=habit_list).ask()
     habit_id = habit_to_mark.split(":")[0].strip()
-
+    # Get the habit from the database
     habit = db.get_habit(habit_id)
     habit_name = habit[1]
     period = habit[2]
@@ -66,7 +69,7 @@ def habit_status():
         last_completion_date = datetime.strptime(last_completion_date, '%Y-%m-%d').date()
     number_of_completions = habit[5]
     days_since_last_completion = 0
-
+    # Check if the habit has been completed within the period
     if last_completion_date is not None:
         days_since_last_completion = (date.today() - last_completion_date).days
 
@@ -84,10 +87,10 @@ def habit_status():
             pass
         else:
             print("You have completed your habit within it's period. It's great, keep going!")
-
+    # If the habit has not been completed yet, print a message
     else:
         print(f"Habit: {habit_name} has not been completed yet.")
-
+    # Print the habit status
     print("=" * 35)
     print("Habit: {}".format(habit_name))
     print("Period: {}".format(period))
@@ -100,7 +103,6 @@ def habit_status():
 def show_habit_streaks():
     """
     This function prints the streaks of all habits.
-    :return:
     """
     habits = db.get_habits()
     streaks = db.get_streaks()
@@ -127,6 +129,10 @@ def show_habit_streaks():
 
 
 def show_habit_completions(habit_id):
+    """
+    This function prints the completions of a habit.
+    :param habit_id: The id of the habit. (int)
+    """
     completions = db.get_habit_completions(habit_id)
     habit_completions = []
 
--- habits.py
+++ habits.py
@@ -15,9 +15,17 @@ class Habit:
 
     def add_habit(self, name, periodicity, creation_date):
         db.__init__()
+        # Check if habit already exists
+        db.c.execute("SELECT * FROM habits WHERE habit_name=?", (name,))
+        row = db.c.fetchone()
+        if row:
+            print(termcolor.colored("Habit already exists!", "red"))
+            return
+        # Add habit to database
         db.c.execute("INSERT INTO habits (habit_name, periodicity, creation_date) VALUES (?, ?, ?)",
                      (name, periodicity, creation_date))
         self.habit_id = db.c.lastrowid
+        # Add habit to streaks table
         db.c.execute("INSERT INTO streaks (habit_id, current_streak, longest_streak) VALUES (?, ?, ?)",
                      (self.habit_id, 0, 0))
         db.conn.commit()
@@ -30,13 +38,16 @@ class Habit:
         self.completion_date = datetime.strptime(completion_date, '%Y-%m-%d').date()
         last_completion_date = db.get_habit(self.habit_id)[4]
         periodicity = db.get_habit_periodicity(self.habit_id)
+        # Check if habit has been completed within the period
         if last_completion_date:
             last_completion_date = datetime.strptime(last_completion_date, '%Y-%m-%d').date()
             if (self.completion_date - last_completion_date).days < periodicity:
                 print(termcolor.colored("Habit has already been marked as complete within it's period!", "red"))
                 return
+        # Add completion to database
         db.c.execute("INSERT INTO completions (habit_id, completion_date) VALUES (?, ?)",
                      (self.habit_id, self.completion_date))
+        # Update habit's last completion date and number of completions
         db.c.execute(
             "UPDATE habits SET last_completion_date=?, number_of_completions=number_of_completions+1 WHERE id=?",
             (self.completion_date, self.habit_id))
@@ -53,20 +64,26 @@ class Habit:
                      (self.habit_id,))
         rows = db.c.fetchall()
         completion_list = []
+        # Convert completion dates to datetime objects
         for row in rows:
             completion_list.append(datetime.strptime(row[0], '%Y-%m-%d').date())
         current_streak = db.get_streaks_for_habit(self.habit_id)[2]
         longest_streak = db.get_streaks_for_habit(self.habit_id)[3]
+        # Check if habit has been completed for the first time
         if len(completion_list) == 1:
             current_streak = 1
             longest_streak = 1
+        # Check if habit has been completed more than once
         else:
             prev_date = completion_list[1]
             current_date = completion_list[0]
+            # Check if habit has been completed within the period
             if (current_date - prev_date).days <= periodicity:
                 current_streak += 1
+                # Check if current streak is the longest streak
                 if current_streak > longest_streak:
                     longest_streak = current_streak
+            # Habit has not been completed within the period
             else:
                 current_streak = 1
 
@@ -83,8 +100,11 @@ class Habit:
         """
         db.__init__()
         self.habit_id = habit_id
+        # Delete habit from habits table
         db.c.execute("""DELETE FROM habits WHERE id = ?""", (self.habit_id,))
+        # Delete habit from streaks table
         db.c.execute("""DELETE FROM streaks WHERE habit_id = ?""", (self.habit_id,))
+        # Delete habit from completions table
         db.c.execute("""DELETE FROM completions WHERE habit_id = ?""", (self.habit_id,))
         db.conn.commit()
         db.conn.close()
