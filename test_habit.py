b2011b9c6703ae9ac2addc427b67531771c1b024
mohamadsolouki <mohamad.solouki@gmail.com>
2023-03-05 15:25:12 +0330

    use datetime instead of time

--- test_habit.py
+++ test_habit.py
@@ -11,51 +11,51 @@ def get_db(name="test.db"):
 
 # test adding 6 predefined habits
 def test_add_habit():
-    hb.add_habit("Study", 1, '2023-01-01', "test.db")
-    hb.add_habit("Workout", 1, '2023-01-05', "test.db")
-    hb.add_habit("Swim", 7, '2023-01-08', "test.db")
-    hb.add_habit("Programming", 1, '2023-01-04', "test.db")
-    hb.add_habit("Travel", 30, '2023-01-02', "test.db")
-    assert db.get_habit(1) == (1, "Study", 1, '2023-01-01', None, 0)
-    assert db.get_habit(2) == (2, "Workout", 1, '2023-01-05', None, 0)
-    assert db.get_habit(3) == (3, "Swim", 7, '2023-01-08', None, 0)
-    assert db.get_habit(4) == (4, "Programming", 1, '2023-01-04', None, 0)
-    assert db.get_habit(5) == (5, "Travel", 30, '2023-01-02', None, 0)
+    hb.add_habit("Study", 1, '2023-01-01 01:00:00', "test.db")
+    hb.add_habit("Workout", 1, '2023-01-05 01:00:00', "test.db")
+    hb.add_habit("Swim", 7, '2023-01-08 01:00:00', "test.db")
+    hb.add_habit("Programming", 1, '2023-01-04 01:00:00', "test.db")
+    hb.add_habit("Travel", 30, '2023-01-02 01:00:00', "test.db")
+    assert db.get_habit(1) == (1, "Study", 1, '2023-01-01 01:00:00', None, 0)
+    assert db.get_habit(2) == (2, "Workout", 1, '2023-01-05 01:00:00', None, 0)
+    assert db.get_habit(3) == (3, "Swim", 7, '2023-01-08 01:00:00', None, 0)
+    assert db.get_habit(4) == (4, "Programming", 1, '2023-01-04 01:00:00', None, 0)
+    assert db.get_habit(5) == (5, "Travel", 30, '2023-01-02 01:00:00', None, 0)
 
 
 # test marking a daily habit as complete for 2 consecutive days
 def test_mark_daily_as_complete_two_consecutive_days():
-    hb.mark_habit_as_complete(1, '2023-01-02', "test.db")
-    hb.mark_habit_as_complete(1, '2023-01-03', "test.db")
-    assert db.get_habit_completions(1) == [('2023-01-02',), ('2023-01-03',)]
+    hb.mark_habit_as_complete(1, '2023-01-02 01:00:00', "test.db")
+    hb.mark_habit_as_complete(1, '2023-01-03 01:00:00', "test.db")
+    assert db.get_habit_completions(1) == [('2023-01-02 01:00:00',), ('2023-01-03 01:00:00',)]
 
 
 def test_mark_daily_as_complete_five_consecutive_days():
-    hb.mark_habit_as_complete(2, '2023-02-02', "test.db")
-    hb.mark_habit_as_complete(2, '2023-02-03', "test.db")
-    hb.mark_habit_as_complete(2, '2023-02-04', "test.db")
-    hb.mark_habit_as_complete(2, '2023-02-05', "test.db")
-    hb.mark_habit_as_complete(2, '2023-02-06', "test.db")
-    assert db.get_habit_completions(2) == [('2023-02-02',), ('2023-02-03',), ('2023-02-04',), ('2023-02-05',),
-                                           ('2023-02-06',)]
+    hb.mark_habit_as_complete(2, '2023-02-02 01:00:00', "test.db")
+    hb.mark_habit_as_complete(2, '2023-02-03 01:00:00', "test.db")
+    hb.mark_habit_as_complete(2, '2023-02-04 01:00:00', "test.db")
+    hb.mark_habit_as_complete(2, '2023-02-05 01:00:00', "test.db")
+    hb.mark_habit_as_complete(2, '2023-02-06 01:00:00', "test.db")
+    assert db.get_habit_completions(2) == [('2023-02-02 01:00:00',), ('2023-02-03 01:00:00',), ('2023-02-04 01:00:00',),
+                                           ('2023-02-05 01:00:00',), ('2023-02-06 01:00:00',)]
 
 
 # test marking a weekly habit as complete for 3 consecutive weeks
 def test_mark_weekly_as_complete_three_consecutive_weeks():
-    hb.mark_habit_as_complete(3, '2023-01-11')
-    hb.mark_habit_as_complete(3, '2023-01-18')
-    hb.mark_habit_as_complete(3, '2023-01-24')
-    assert db.get_habit_completions(3) == [('2023-01-11',), ('2023-01-18',), ('2023-01-24',)]
+    hb.mark_habit_as_complete(3, '2023-01-11 01:00:00')
+    hb.mark_habit_as_complete(3, '2023-01-18 01:00:00')
+    hb.mark_habit_as_complete(3, '2023-01-24 01:00:00')
+    assert db.get_habit_completions(3) == [('2023-01-11 01:00:00',), ('2023-01-18 01:00:00',), ('2023-01-24 01:00:00',)]
 
 
 # test marking a daily habit as complete for 6 times which has current streak of 0 and longest streak of 4
 def test_mark_habit_as_complete():
-    hb.mark_habit_as_complete(4, "2023-01-08", "test.db")
-    hb.mark_habit_as_complete(4, "2023-01-09", "test.db")
-    hb.mark_habit_as_complete(4, "2023-01-10", "test.db")
-    hb.mark_habit_as_complete(4, "2023-01-11", "test.db")
-    hb.mark_habit_as_complete(4, "2023-01-13", "test.db")
-    hb.mark_habit_as_complete(4, "2023-01-14", "test.db")
+    hb.mark_habit_as_complete(4, "2023-01-08 01:00:00", "test.db")
+    hb.mark_habit_as_complete(4, "2023-01-09 01:00:00", "test.db")
+    hb.mark_habit_as_complete(4, "2023-01-10 01:00:00", "test.db")
+    hb.mark_habit_as_complete(4, "2023-01-11 01:00:00", "test.db")
+    hb.mark_habit_as_complete(4, "2023-01-13 01:00:00", "test.db")
+    hb.mark_habit_as_complete(4, "2023-01-14 01:00:00", "test.db")
     assert db.get_updated_streaks(4) == (4, 4, 0, 4)
 
 
@@ -66,7 +66,7 @@ def test_delete_habit():
 
 
 def test_get_habit():
-    assert db.get_habit(2) == (2, "Workout", 1, '2023-01-05', None, 0)
-    assert db.get_habit(3) == (3, "Swim", 7, '2023-01-08', None, 0)
-    assert db.get_habit(4) == (4, "Programming", 1, '2023-01-04', None, 0)
-    assert db.get_habit(5) == (5, "Travel", 30, '2023-01-02', None, 0)
+    assert db.get_habit(2) == (2, "Workout", 1, '2023-01-05 01:00:00', None, 0)
+    assert db.get_habit(3) == (3, "Swim", 7, '2023-01-08 01:00:00', None, 0)
+    assert db.get_habit(4) == (4, "Programming", 1, '2023-01-04 01:00:00', None, 0)
+    assert db.get_habit(5) == (5, "Travel", 30, '2023-01-02 01:00:00', None, 0)
